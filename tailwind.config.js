b3f870d2346e3faac60167071721fd821ccfa22e
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-12-30 00:08:02 +0000

    Update Tailwind CSS configuration, add login, sign-up templates, and creation of lanch page for  the library management system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/**/*.html",
-    "./static/css/**/*.css",
+    "./app/templates/**/*.html",
+    "./app/static/css/**/*.css",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'library-intro': "url('../images/introduction/image (1).jpg')",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
   },
   plugins: [],
 }
