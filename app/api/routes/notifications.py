9ece873e0b55c1243e6d40f025a0523205260fdd
MarvelousMarvin7 <marvinagyei3@gmail.com>
2025-01-26 12:14:14 +0000

    Add Swagger documentation for notifications API endpoints

--- app/api/routes/notifications.py
+++ app/api/routes/notifications.py
@@ -4,6 +4,7 @@
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.api.routes import api
 from flask import jsonify, Response, abort, make_response
+from flasgger import swag_from
 from typing import Union
 from models import storage
 from models.notification import Notification
@@ -11,6 +12,7 @@ from models.notification import Notification
 
 @api.route('/notifications', methods=['GET'], strict_slashes=False)
 @jwt_required()
+@swag_from('documentation/notification/get_all.yml')
 def get_user_notifications() -> Union[Response, dict]:
     """Get list of all notifications for
     the current user"""
@@ -26,6 +28,7 @@ def get_user_notifications() -> Union[Response, dict]:
 
 @api.route('/notifications/<notification_id>', methods=['GET'], strict_slashes=False)
 @jwt_required()
+@swag_from('documentation/notification/get_by_id.yml')
 def get_notification(notification_id: str) -> Union[Response, dict]:
     """Get notification by id for a user"""
     user_id = get_jwt_identity()
@@ -37,6 +40,7 @@ def get_notification(notification_id: str) -> Union[Response, dict]:
 
 @api.route('/notifications/<notification_id>', methods=['DELETE'], strict_slashes=False)
 @jwt_required()
+@swag_from('documentation/notification/delete.yml')
 def delete_notification(notification_id: str) -> Union[Response, dict]:
     """Delete notification by id"""
     user_id = get_jwt_identity()
@@ -50,6 +54,7 @@ def delete_notification(notification_id: str) -> Union[Response, dict]:
 
 @api.route('/notifications/<notification_id>', methods=['PUT'], strict_slashes=False)
 @jwt_required()
+@swag_from('documentation/notification/mark_read.yml')
 def mark_notification_as_read(notification_id: str) -> Response:
     """Current user update notifications as read"""
     user_id = get_jwt_identity()
